4906eef1fbb0993023dd2f814935141798a984bf
tomokazutakahashi4 <takahashitomokazu4@gmail.com>
2020-11-21 00:28:43 +0900

    Realtime changes、ソートを追加

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -5,11 +5,25 @@ import 'package:todo_app/todo.dart';
 class MainModel extends ChangeNotifier{
   List<Todo>todoList = [];
 
+  //One-time Read
   Future getTodoList() async{
     final snapshot = await FirebaseFirestore.instance.collection('todoList').get();
     final docs = snapshot.docs;
     final todoList = docs.map((doc) => Todo(doc)).toList();
     this.todoList = todoList;
     notifyListeners();
+  }
+  //Realtime changes・・・エミュレータにリアルタイム反映させる
+  void getTodoListRealtime(){
+    final snapdhots = FirebaseFirestore.instance.collection('todoList').snapshots();
+    snapdhots.listen((snapdhot){
+      final docs = snapdhot.docs;
+      final todoList = docs.map((doc) => Todo(doc)).toList();
+      //ソート
+      todoList.sort((a, b) => a.createdAt.compareTo(b.createdAt));
+
+      this.todoList = todoList;
+      notifyListeners();
+    });
   }
 }
